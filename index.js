a5604f6aa915f4106304292976e9006ff1acae4b
RohitSathya <156504207+RohitSathya@users.noreply.github.com>
2024-08-12 09:37:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ const router=require('./Routes/Routes')
 
 
 app.use(express.json())
-app.use(cors({
-  origin: 'https://ecomerce-mern-royo.vercel.app', // allow this specific origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // specify the allowed methods
-  credentials: true // allow cookies to be sent
-}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://ecomerce-mern-royo.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
+});
+
 app.use("/product",router)
 app.get("/",async(req,res)=>{
     res.json("hi")
