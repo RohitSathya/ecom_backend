5374571beb5c6ee43423e014416495484a460fdb
RohitSathya <156504207+RohitSathya@users.noreply.github.com>
2024-08-18 13:54:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const express=require('express')
-const cors=require('cors')
-const app=express()
-const mongoose=require('mongoose')
-const router=require('./Routes/Routes')
-const pr=require('./Routes/productRoutes')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mongoose = require('mongoose');
+const router = require('./Routes/Routes');
+const pr = require('./Routes/productRoutes');
 
+app.use(express.json());
 
-app.use(express.json())
+const allowedOrigins = [
+  'https://ecomerce-mern-royo.vercel.app',
+  'https://affiliate-site.vercel.app'
+];
+
 app.use((req, res, next) => {
-  const allowedOrigins = ['https://ecomerce-mern-royo.vercel.app', 'https://affiliate-site.vercel.app'];
   const origin = req.headers.origin;
-
+  
   if (allowedOrigins.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
@@ -18,7 +22,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
-  
+
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
@@ -26,16 +30,15 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use("/product", router);
+app.use('/pro', pr);
 
-app.use("/product",router)
-app.use('/pro',pr)
-app.get("/",async(req,res)=>{
-    res.json("hi")
-})
+app.get("/", async (req, res) => {
+  res.json("hi");
+});
 
-                           
-mongoose.connect("mongodb+srv://admin:sunsetwest1234RRR@royoapi.3qmdrjq.mongodb.net/ecomeerce?retryWrites=true&w=majority").then(()=>{
-    console.log('database connected')
-  app.listen(8081,()=>console.log('running'))
- 
-})
+mongoose.connect("mongodb+srv://admin:sunsetwest1234RRR@royoapi.3qmdrjq.mongodb.net/ecomeerce?retryWrites=true&w=majority")
+  .then(() => {
+    console.log('database connected');
+    app.listen(8081, () => console.log('running'));
+  });
