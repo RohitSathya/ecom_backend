e08144cd971920a7709f69b10d45a199ecce488e
RohitSathya <156504207+RohitSathya@users.noreply.github.com>
2024-08-18 20:09:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://www.explorepricing.com');
+  res.header('Access-Control-Allow-Origin', 'https://explorepricing.com');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
