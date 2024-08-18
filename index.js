abd5b28072e4d7832204e867c93a5abce49c29f7
RohitSathya <156504207+RohitSathya@users.noreply.github.com>
2024-08-18 13:43:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,24 @@ const pr=require('./Routes/productRoutes')
 
 app.use(express.json())
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://ecomerce-mern-royo.vercel.app');
+  const allowedOrigins = ['https://ecomerce-mern-royo.vercel.app', 'https://affiliate-site.vercel.app'];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
+  
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
+
   next();
 });
+
 
 app.use("/product",router)
 app.use('/pro',pr)
